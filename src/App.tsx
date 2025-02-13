26e91b66615ca649fd1b049a540be8268e163e09
SolSoc <socrates.nft@gmail.com>
2025-02-13 03:41:37 -0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import HomePage from '@/pages/HomePage'
+import LaunchPage from '@/pages/LaunchPage'
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/launch" element={<LaunchPage />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
